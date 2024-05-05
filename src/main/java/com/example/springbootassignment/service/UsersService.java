173fbdb761501973aaa70952ce4a0e9822f55ec8
Manas Arora <126653235+manasarora-04@users.noreply.github.com>
2024-05-05 21:45:06 +0530

    logs removed

--- src/main/java/com/example/springbootassignment/service/UsersService.java
+++ src/main/java/com/example/springbootassignment/service/UsersService.java
@@ -2,8 +2,6 @@ package com.example.springbootassignment.service;
 
 import com.example.springbootassignment.Repository.UsersRepository;
 import com.example.springbootassignment.api.model.Users;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
